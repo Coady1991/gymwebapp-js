ff146fb54677e774a92b91a0051402f65f810378
Niall Coady <niall_coady7@hotmail.com>
2017-07-24 22:03:18 +0100

    Member can now add an Assessment

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -2,7 +2,9 @@
 
 const logger = require('../utils/logger');
 const accounts = require('./accounts.js');
+const assessmentStore = require('../models/assessment-store');
 const analytics = require('../utils/analytics');
+//const dateFormat = require('dateformat');
 const uuid = require('uuid');
 
 const dashboard = {
@@ -18,9 +20,34 @@ const dashboard = {
       BMI: BMI,
       BMICategory: BMICategory,
       idealBodyWeight: idealBodyWeight,
+      assessments: assessmentStore.getUserAssessments(loggedInUser.id),
     };
     logger.info('about to render');
     response.render('dashboard', viewData);
+  },
+  deleteAssessment(request, response) {
+    const assessmentId = request.params.id;
+    logger.debug(`Deleting Assessment ${assessmentId} from Dashboard`);
+    assessmentStore.deleteAssessment(assessmentId);
+    response.redirect('/dashboard/');
+  },
+  addAssessment(request, response) {
+    const loggedInUser = accounts.getCurrentUser(request);
+    const newAssessment = {
+      id: uuid(),
+      userid: loggedInUser.id,
+      //date: dateformat(new Date(), 'dd-mm-yy'), //https://stackoverflow.com/questions/3552461/how-to-format-a-javascript-date
+      weight: request.body.weight,
+      chest: request.body.chest,
+      thigh: request.body.thigh,
+      upperArm: request.body.upperArm,
+      waist: request.body.waist,
+      hips: request.body.hips,
+      comment: '',
+    };
+    logger.debug('Creating a new assessment', newAssessment);
+    assessmentStore.addAssessmentList(newAssessment);
+    response.redirect('/dashboard/');
   },
 };
 
---
+++ models/assessment-store.js
@@ -0,0 +1,40 @@
+'use strict';
+
+const _ = require('lodash');
+const JsonStore = require('./json-store');
+
+const assessmentStore = {
+
+  store: new JsonStore('./models/assessment-store.json', { assessmentsList: [] }),
+  collection: 'assessmentsList',
+
+  getAllAssessments() {
+    return this.store.findAll(this.collection);
+  },
+
+  getAssessmentList(id) {
+    return this.store.findOneBy(this.collection, { id: id });
+  },
+
+  getUserAssessments(userid) {
+    return this.store.findBy(this.collection, { userid: userid });
+  },
+
+  addAssessmentList(assessment) {
+    this.store.add(this.collection, assessment);
+    this.store.save();
+  },
+
+  /*addAssessment(id, assessment) {
+    const assessmentNew = this.getAssessmentList(id);
+    assessmentNew.assessments.push(assessment);
+    this.store.save();
+  },*/
+
+  deleteAssessment(assessment) {
+    this.store.remove(this.collection, assessment);
+    this.store.save();
+  },
+};
+
+module.exports = assessmentStore;
